7236f606e3abedeb2625f1ede6359dc18d4732d9
MuhammadAnugrahSupriyandi <anugrahs15042004@gmail.com>
2024-11-22 16:56:50 +0700

    kesehatan

--- asrama/connect.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $conn=mysqli_connect('localhost','root','','asrama');
-    if (!$conn){
-        die('tidak terkoneksi'.mysqli_connect_error());
-    }
-?>
---
+++ home/index.php
@@ -0,0 +1,27 @@
+<?php
+    include '../connect.php';
+    session_start();
+    if ($_SESSION['logged_in']== false){
+        header('location:../login/login_warga.php');
+    }
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>asrama</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet">
+    <style>
+        <?php include 'assets/style.css'; ?> /* Jika style dipisahkan */
+    </style>
+</head>
+
+<body>
+    <?php require 'assets/header.php'; ?>
+    <h1>SELAMAT DATANG <?=$_SESSION['nama']?></h1>
+</body>
+</html>
